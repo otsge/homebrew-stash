81812102ef94ae4be5ae79bf7efeac5a00ea94b8
OTSGE <199041525+otsge@users.noreply.github.com>
2026-01-09 12:53:03 +0000

    Initial add of teldrive

---
+++ Formula/teldrive.rb
@@ -0,0 +1,34 @@
+class Teldrive < Formula
+  desc "Organizer for your telegram files"
+  homepage "https://github.com/tgdrive/teldrive"
+  url "https://github.com/tgdrive/teldrive.git",
+        tag:      "1.7.4",
+        revision: "ad2611d3cfa3c46897d1bb823415595317ae2455"
+  license "MIT"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "go" => :build
+  depends_on "go-task" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    ENV["GO111MODULE"] = "on"
+    ldflags = %W[
+      -extldflags=-static
+      -s -w
+      -X github.com/tgdrive/teldrive/internal/version.Version=#{version}
+      -X github.com/tgdrive/teldrive/internal/version.CommitSHA=#{Utils.git_short_head(length: 7)}
+    ]
+    system "task", "ui"
+    system "task", "gen"
+    system "go", "build", "-trimpath", *std_go_args(ldflags:)
+  end
+
+  test do
+    system bin/"teldrive", "version"
+  end
+end
