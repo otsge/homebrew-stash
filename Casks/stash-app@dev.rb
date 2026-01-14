159cbd55f4212134a8b2696d20c15d0d33fdffe0
OTSGE <199041525+otsge@users.noreply.github.com>
2026-01-14 02:35:35 +0000

    stash-app@dev 0.30.1-25-gb4969add

--- Casks/stash-app@dev.rb
+++ Casks/stash-app@dev.rb
@@ -1,5 +1,5 @@
 cask "stash-app@dev" do
-  version "0.30.1-24-g6049b21d"
+  version "0.30.1-25-gb4969add"
   sha256 :no_check
 
   url "https://github.com/stashapp/stash/releases/download/latest_develop/Stash.app.zip",
