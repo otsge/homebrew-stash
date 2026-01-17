23d12a534afe4fbf1f402835990a7c95073ad12d
OTSGE <199041525+otsge@users.noreply.github.com>
2026-01-17 09:46:32 +0000

    taproom: update 0.5.0 bottle.

--- Formula/taproom.rb
+++ Formula/taproom.rb
@@ -8,12 +8,12 @@ class Taproom < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/otsge/stash"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "746325dddae3d390ce3463d18e8aa1b112eeeaa81ae539327d2f44c7c49cf9ea"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "01585fd1778b2ed28b93157f63f7cfb2f5151ea8ddcb84b3e6d55ec8534291a5"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2a65440ef012ca2fb550ab82ccf40b882bd318697a247e97519dac46e6780919"
-    sha256 cellar: :any_skip_relocation, sequoia:       "3ff2d783badb23446922ef421d70520494113198d3363d84c03d92fe486fb0ac"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "17411805d2aef42370ed4e7e9e1f0ebab889e3a6d748833ff428d3bcd3b3efc2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a29a8a33edafc7bce53a197fa77570dc096ee9dfeb16ba98648b10f73bc307e5"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d05d7df12fc2617a301f17c53901e96308e4bec983a23961b4e4ce4ff5fe81b2"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "78d3c4b7602cbe0091d27750855e70e9b94a3e1ad64300ee6e14b5a62de11e19"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "08582aa6d00cff386919d3bd7a50c4f52cf09e956b3cde74cdca34313017596d"
+    sha256 cellar: :any_skip_relocation, sequoia:       "b0d2863f1a8a3f5a462651800de7b9fa99f74767f8b5a5f58d42fa77b9204239"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3a65e0ad37b204f6d067a1946f9d2f2c430b16e998a85f9bdbd38a7c5d89bb79"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "77ee493b4a7ba0bc0164a362d48858e35267550081052f38034a8d02933013f5"
   end
 
   depends_on "go" => :build
