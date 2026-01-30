a2e3a34400775ad7e5bebf0e92ce787263e1b109
OTSGE <199041525+otsge@users.noreply.github.com>
2026-01-30 21:53:12 +0000

    rclone: update 1.73.0 bottle.

--- Formula/rclone.rb
+++ Formula/rclone.rb
@@ -8,12 +8,12 @@ class Rclone < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/otsge/stash"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "178643c1c6182bb9d1ba3fd0633197dfe0acd07152fb7ec2517a6c16302d60a9"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2e76d05f68a61f60a80ddf30731c18cced2a2767a363060e4de48696956630fd"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6fea20ec6c5ebdc5fbff354335ddb14fdb88015a54c8e362f25fa9b92f169519"
-    sha256 cellar: :any_skip_relocation, sequoia:       "f63224a77ea03066887f99fdcc214397147297ab4a22b52de0259206b06ba210"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "581dd8e0a9b3e27a962222eeb5dd30106055807bb579e032e34b1436e9d81375"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "48a611868440586ae715edfef4cf70401368f31442777348e2afe564b50ad596"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9f45046536b286eff191d90638d26a8c0aee837988d317394790d350f8a9352d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ce8cf200c1195e8389ab35a2d4274242d9f247cae45d997364512edf88d46f7b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2f5a7262bfcc4b6c92b1564580ab126f1b31f52786564a0546232d874ff16b31"
+    sha256 cellar: :any_skip_relocation, sequoia:       "c82e73b47c3b33bb33e7f7d69d36812c638259ea22af5aa94a184681cb82f169"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c1b7980dc0676eb98458b889a488a67273258c875f1aae4b30fe13edb6000b0b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "da76e4a9cc9d75149855dfe9a8a0f987ff9c8faeeae1ee41eb94a081068e458c"
   end
 
   depends_on "go" => :build
