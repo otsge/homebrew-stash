53b93478c83524ce6f9b039700bd24a3bd4878ce
OTSGE <199041525+otsge@users.noreply.github.com>
2026-02-03 19:28:53 +0000

    teldrive: update 1.8.1 bottle.

--- Formula/teldrive.rb
+++ Formula/teldrive.rb
@@ -8,12 +8,12 @@ class Teldrive < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/otsge/stash"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6b71ccdd0bdd4d3dbc04b3402c4635997ffa25022a408dd3cc2fc70a565601ff"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "500c190f7d4ad26e0bcd6eae7d40a2851e07f49145a541402374f497460f9c4b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "77dbef07855e6355f98a19c415125c42014fa702e9cf16c2965eeddac4a9fdaf"
-    sha256 cellar: :any_skip_relocation, sequoia:       "f51f08101c5b8b34eb24c5e5ac906aa71f75719bab1ba1e9f390c3bb08b7c41b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f8f267644774beb38d40eaff368e9faf727e5209507a2c87ce480bc42099e452"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6893a8a1b17b56776d0ae05890fdd9b888ce14140aff24bbe06b3ac688eca147"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d06036e72859fe171b7995e50ce640322ca63ff60e9958fe96c3a281b8966c66"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7ecfdaf2d43d6fcadd889643e437ae6272787a359447501ffadebee911c11159"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d7d33debf85a60bd03477e7beff4c059b1baa6fa16c21b26665bb03224902740"
+    sha256 cellar: :any_skip_relocation, sequoia:       "4bbc3e9e73184ff9ddfb7eff66b0dae16985dfd39ae468eb158df005cc09d1dc"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "4afe08c6478dd9e71ae2715305bc231fd4241c02e93614499da2281164673246"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a148d4f6ca5cb416b1f5b2fab2fa0e06d02c3ebf6edf311651c291de82baf649"
   end
 
   depends_on "go" => :build
