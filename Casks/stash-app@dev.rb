1322a235b400851dfa4121716eafa9a38213f639
OTSGE <199041525+otsge@users.noreply.github.com>
2026-03-09 21:48:10 +0000

    stash-app@dev 0.30.1-116-gcacaf363
    Closes #108.
    
    Signed-off-by: OTSGE <199041525+otsge@users.noreply.github.com>

--- Casks/stash-app@dev.rb
+++ Casks/stash-app@dev.rb
@@ -1,5 +1,5 @@
 cask "stash-app@dev" do
-  version "0.30.1-115-g74a8f2e5"
+  version "0.30.1-116-gcacaf363"
   sha256 :no_check
 
   url "https://github.com/stashapp/stash/releases/download/latest_develop/Stash.app.zip",
