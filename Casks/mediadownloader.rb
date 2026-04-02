b8a5e8fff7992f79c1707d0838c4eb615042e2f9
John Faust <45057319+slickag@users.noreply.github.com>
2026-04-02 15:59:32 -0700

    Update Link to MediaDownloader

--- Casks/mediadownloader.rb
+++ Casks/mediadownloader.rb
@@ -2,7 +2,7 @@ cask "mediadownloader" do
   version "5.4.8"
   sha256 "06fda5a74f0a07ffd91f0b9a9ada99796c1199f5733b07a40d31ff122f6004d3"
 
-  url "https://github.com/mhogomchungu/media-downloader/releases/download/#{version}/MediaDownloaderQt6-#{version}.dmg"
+  url "https://github.com/mhogomchungu/media-downloader/releases/download/#{version}/MediaDownloaderQt6-arm64-#{version}.dmg"
   name "Media Downloader"
   desc "Qt/C++ front end to yt-dlp, youtube-dl, gallery-dl, and more"
   homepage "https://github.com/mhogomchungu/media-downloader"
