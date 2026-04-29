fc63f59a8d79862a633410d503fe0500d4331dd2
OTSGE <199041525+otsge@users.noreply.github.com>
2026-04-29 19:55:16 +0000

    mediadownloader@git 5.5.3.202604291938

--- Casks/mediadownloader@git.rb
+++ Casks/mediadownloader@git.rb
@@ -1,5 +1,5 @@
 cask "mediadownloader@git" do
-  version "5.5.2.202604290655"
+  version "5.5.3.202604291938"
   sha256 :no_check
 
   url "https://github.com/mhogomchungu/media-downloader-git/releases/download/0.0.0/MediaDownloaderQt6.arm64.git.dmg"
