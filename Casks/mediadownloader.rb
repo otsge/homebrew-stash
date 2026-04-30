8e2d84cb893013f48ea9cb2bb5344c2f338c4286
OTSGE <199041525+otsge@users.noreply.github.com>
2026-04-30 19:51:41 +0000

    mediadownloader 5.6.0
    Closes #192.
    
    Signed-off-by: OTSGE <199041525+otsge@users.noreply.github.com>

--- Casks/mediadownloader.rb
+++ Casks/mediadownloader.rb
@@ -1,6 +1,6 @@
 cask "mediadownloader" do
-  version "5.5.3"
-  sha256 "3ddb380f0cd575df709306c39c6b6f2e787c5b306d93e0005f5892986df295b8"
+  version "5.6.0"
+  sha256 "50bc952f85f52cc842c2b028453959e4d3429442cfbfa769efee29c291f49c35"
 
   url "https://github.com/mhogomchungu/media-downloader/releases/download/#{version}/MediaDownloaderQt6-arm64-#{version}.dmg"
   name "Media Downloader"
